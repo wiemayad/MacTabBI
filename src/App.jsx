8a07c5c9e7ea3c438e38a6ca438c98f5236cae4a
Wiwa ❤ <wiem.ayad@gmail.com>
2023-11-08 16:51:13 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 
-import index from './navigation/index'
-import Home from './pages/Home/Home'
-import NavBar1 from './components/Home/NavBar1'
 
 import Navigation from './navigation/index'
 
